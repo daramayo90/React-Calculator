32c832b83a572874f35d96ac0d5efe2c3fa395d9
daramayo90 <da.aramayo1990@gmail.com>
2022-02-28 18:13:48 -0500

    more complex functionalities have been added

--- src/App.js
+++ src/App.js
@@ -6,45 +6,61 @@ function App() {
   const [number1, setNumber1] = useState("0");
   const [number2, setNumber2] = useState("");
   const [currentOperation, setCurrentOperation] = useState("");
-  const [result, setResult] = useState(0);
+  const [result, setResult] = useState(0, false);
+  const [hasResult, setHasResult] = useState(false);
 
   function clickNumber(val) {
-    if (number1 === "0" && !currentOperation) {
-      setNumber1("" + val);
-
-    } else if (currentOperation && !(val === "." && number2.includes("."))) {
-      setNumber2(number2 + val);
-
-    } else if (!currentOperation && !(val === "." && number1.includes("."))) {
-      setNumber1(number1 + val);
+    if (!hasResult) {
+      if (number1 === "0" && !currentOperation) {
+        setNumber1("" + val);
+  
+      } else if ( currentOperation && !(val === "." && number2.includes("."))) {
+        setNumber2(number2 + val);
+  
+      } else if (!currentOperation && !(val === "." && number1.includes("."))) {
+        setNumber1(number1 + val);
+      }
     }
   }
 
   function clickOperation(val) {
-    setCurrentOperation(val);
+    if (!hasResult && !number2) {
+      setCurrentOperation(val);
+    }
   }
 
   function getResult() {
     if (!result) {
       switch (currentOperation) {
         case "+":
-          console.log("Result: " +  typeof(result))
           setResult(Number(number1) + Number(number2));
-          console.log("Result: " +  typeof(result))
+          setHasResult(true);
           break;
 
         case "-":
           setResult(Number(number1) - Number(number2));
+          setHasResult(true);
           break;
 
         case "*":
-          console.log(number1 * number2)
           setResult(Number(number1) * Number(number2));
+          setHasResult(true);
           break;
 
         case "/":
           setResult(Number(number1) / Number(number2));
+          setHasResult(true);
           break;
+      }
+    }
+  }
+
+  function delNumber() {
+    if (!hasResult) {
+      if (!currentOperation) {
+        setNumber1(number1.slice(0, -1))
+      }else {
+        setNumber2(number2.slice(0, -1))
       }
     }
   }
@@ -54,6 +70,7 @@ function App() {
     setNumber2("");
     setCurrentOperation("");
     setResult(0);
+    setHasResult(false);
   }
 
   return (
@@ -62,12 +79,17 @@ function App() {
 
       <div className="calculator-wrap">
         <div className="output">
-          <div className="previous-operand">{currentOperation ? number1 + currentOperation : ""} </div>
-          <div className="current-operand">{result ? result : (!currentOperation ? number1 : number2)}</div>
+          <div className="previous-operand">
+            {!hasResult ? (currentOperation ? number1 + currentOperation : "") : number1 + currentOperation + number2 + "="}
+          </div>
+          
+          <div className="current-operand">
+            {hasResult ? result : (!currentOperation ? number1 : number2)}
+          </div>
         </div>
 
         <button onClick={allClear} className="span-two">AC</button>
-        <button onClick={() => { }}>DEL</button>
+        <button onClick={delNumber}>DEL</button>
         <button onClick={() => { clickOperation("/") }}>/</button>
         <button onClick={() => { clickNumber(7) }}>7</button>
         <button onClick={() => { clickNumber(8) }}>8</button>
